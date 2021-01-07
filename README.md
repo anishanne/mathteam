08a76ee5a256bf6706ca307984bf652482a46698
Anish <anishanne1@gmail.com>
2021-01-06 19:22:51 -0500

    Odd Numbers

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Solutions for the Math Team problems.
 
 ## Index
-
+[1/6/2021 DUSO Team Problem #3](solutions/oddnumbers.py) - [Run On Repl.it](https://repl.it/@anishanne/Math-Team-Odd-Numbers)
 
 ## Credits
 Made by [Anish](https://github.com/anishanne) & [Robert](https://github.com/ArcWand06)
--- solutions/oddnumbers.py
+++ solutions/oddnumbers.py
@@ -1,20 +1,19 @@
 # 1/6/2021 DUSO Team Problem #3
 
-
 import math
-print(".run")
+print("Starting Up")
 
 num = 14
 n = math.factorial(num)
 count = 0
-print(n)
+print(F"Calclating odd factors for {n}.")
 
-for x in range(1, int(n**0.5)+1):
-  if n % x == 0:
-    if(x % 2 == 1):
+for x in range(1, int(n**0.5)+1): # Goes through every number between 1 & the square root of n + 1.
+  if n % x == 0: # if n / x (will be every number above) has a remainder of 0, continue
+    if(x % 2 == 1): # if x divided by 2 has a reminder of 1, that means its odd, add to the count.
       count += 1
-    if(n % x == 0 and (n/x)%2==1):
+    if(n/x)%2==1): # If n/x is odd, add to the count.
       count += 1
 
 print("\n\n")
-print("answer:" + str(count))
+print("Answer:" + str(count))
